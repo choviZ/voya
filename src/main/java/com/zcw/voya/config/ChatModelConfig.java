d142fe2d9f8c49d9e01073a979c2264f6f0b4e9a
choviZ <702381764@qq.com>
2025-09-05 11:42:49 +0800

    feat:记录每次AI消耗的token

---
+++ src/main/java/com/zcw/voya/config/ChatModelConfig.java
@@ -0,0 +1,32 @@
+package com.zcw.voya.config;
+
+import dev.langchain4j.model.chat.listener.ChatModelListener;
+import dev.langchain4j.model.chat.listener.ChatModelResponseContext;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.output.TokenUsage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 模型配置
+ */
+@Configuration
+@Slf4j
+public class ChatModelConfig {
+
+    @Bean
+    ChatModelListener chatModelListener() {
+        return new ChatModelListener(){
+            @Override
+            public void onResponse(ChatModelResponseContext responseContext) {
+                // 记录每次请求消耗的Token
+                ChatResponse chatResponse = responseContext.chatResponse();
+                TokenUsage tokenUsage = chatResponse.tokenUsage();
+                log.info("输入的token：{}", tokenUsage.inputTokenCount());
+                log.info("输出的token：{}", tokenUsage.outputTokenCount());
+                log.info("总token：{}", tokenUsage.totalTokenCount());
+            }
+        };
+    }
+}
